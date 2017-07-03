256d7a37b4eaf85a97668ab3bd8eb56b5a4ee9f8
abietic <iamyhc@live.cn>
2017-07-03 20:27:38 +0800

    Create newclient1.c

---
+++ GetNumber/newclient1.c
@@ -0,0 +1,80 @@
+ #include <stdio.h>
+ #include <sys/socket.h>
+ #include <sys/types.h>
+ #include <string.h>
+ #include <netinet/in.h>
+ #include <stdlib.h>
+ #include <errno.h>
+ #include <unistd.h>
+ #include <arpa/inet.h>
+ #include "vip.h"
+
+ #define MAXLINE 1024
+int main()
+{
+  char* servInetAddr = "127.0.0.1";
+  int listenfd , connfd , socketfd;
+  struct sockaddr_in sockaddr_get , sockaddr_send;
+  char buff[MAXLINE] , recvline[MAXLINE];
+  char sendline[MAXLINE] = {"10.00.00"};
+  int n;
+
+  while (1)
+  {
+    //printf("Had sent message to server\n");
+
+    int isvp = vip(sendline);
+/***************************************************
+*
+*
+* daimaweizhi
+*
+*
+****************************************************/
+    socketfd = socket(AF_INET , SOCK_STREAM , 0);
+    memset(&sockaddr_send , 0 , sizeof(sockaddr_send));
+    sockaddr_send.sin_family = AF_INET;
+    sockaddr_send.sin_port = htons(10004);
+    inet_pton(AF_INET , servInetAddr , &sockaddr_send.sin_addr);
+
+if ((connect(socketfd , (struct sockaddr*)&sockaddr_send , sizeof(sockaddr_send))) < 0 )
+    {
+      printf("connect error%s errno: %d\n",strerror(errno) , errno);
+      exit(0);
+    }
+
+    if((send(socketfd,sendline,strlen(sendline),0)) < 0)
+    {
+    printf("send mes error: %s errno : %d",strerror(errno),errno);
+    exit(0);
+    }
+    close(socketfd);
+
+    memset(&sockaddr_get , 0 , sizeof(sockaddr_get));
+    sockaddr_get.sin_family = AF_INET;
+    sockaddr_get.sin_addr.s_addr = htonl(INADDR_ANY);
+    sockaddr_get.sin_port = htons(10005);
+    listenfd = socket(AF_INET , SOCK_STREAM , 0);
+
+    bind(listenfd,(struct sockaddr *) &sockaddr_get,sizeof(sockaddr_get));
+    listen(listenfd,1024);
+    if((connfd = accept(listenfd,(struct sockaddr*)NULL,NULL))==-1)
+    {
+    printf("accpet socket error: %s errno :%d\n",strerror(errno),errno);
+    continue;
+    }
+    n = recv(connfd,buff,MAXLINE,0);
+    buff[n] = '\0';
+    //printf("recv msg from client:%s",buff);
+/*******************************************************
+*
+*
+*daimaweizhi
+*
+*
+*******************************************************/
+    print(buff , isvp);
+    close(connfd);
+    close(listenfd);
+  }
+}
