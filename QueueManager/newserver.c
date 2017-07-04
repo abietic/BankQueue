2207e76a4f90792c21c87f854071363ac90fdf6a
abietic <iamyhc@live.cn>
2017-07-04 16:17:49 +0800

    Update newserver.c

--- QueueManager/newserver.c
+++ QueueManager/newserver.c
@@ -9,6 +9,7 @@
 #include <arpa/inet.h>
 #include "newserversfunc.h"
 #include "savecomment.h"
+#include "toreceive.h"
 
 #define MAXLINE 1024
 
@@ -17,61 +18,16 @@
 
 int main()
 {
- char* servInetAddr = "127.0.0.1";
- int listenfd , connfd , socketfd;
- struct sockaddr_in sockaddr_get , sockaddr_send;
- char buff[MAXLINE] , sendline[MAXLINE] , recvline[MAXLINE];
- int n;
+
+char buff[MAXLINE];
  Creatqueue();
  inited();
+ int listenfd = initiateforreceive(10004);
  while (1)
  {
-   //printf("hallo\n" );
-   memset(&sockaddr_get , 0 , sizeof(sockaddr_get));
-   sockaddr_get.sin_family = AF_INET;
-   sockaddr_get.sin_addr.s_addr = htonl(INADDR_ANY);
-   sockaddr_get.sin_port = htons(10004);
-   listenfd = socket(AF_INET , SOCK_STREAM , 0);
-
-   bind(listenfd,(struct sockaddr *) &sockaddr_get,sizeof(sockaddr_get));
-   listen(listenfd,1024);
-   printf("");///////
-   if((connfd = accept(listenfd,(struct sockaddr*)NULL,NULL))==-1)
-   {
-   printf("accpet socket error: %s errno :%d\n",strerror(errno),errno);
+   if (toreceive(buff , listenfd) == 0)
    continue;
-   }
-   n = recv(connfd,buff,MAXLINE,0);
-   buff[n] = '\0';
-
-   //printf("recv msg from client:%s",buff);
-
-   close(connfd);
-   close(listenfd);
-
    ChoseFunction(buff);
-   printf("");//////
-   /*printf("send message to server\n");
-
-   fgets(sendline , 1024 , stdin);
-
-   socketfd = socket(AF_INET , SOCK_STREAM , 0);
-   memset(&sockaddr_send , 0 , sizeof(sockaddr_send));
-   sockaddr_send.sin_family = AF_INET;
-   sockaddr_send.sin_port = htons(10005);
-   inet_pton(AF_INET , servInetAddr , &sockaddr_send.sin_addr);
-
-   if ((connect(socketfd , (struct sockaddr*)&sockaddr_send , sizeof(sockaddr_send))) < 0 )
-   {
-     printf("connect error%s errno: %d\n",strerror(errno) , errno);
-     exit(0);
-   }
-
-   if((send(socketfd,sendline,strlen(sendline),0)) < 0)
-   {
-   printf("send mes error: %s errno : %d",strerror(errno),errno);
-   exit(0);
-   }
-   close(socketfd);*/
  }
+ close(listenfd);
 }
